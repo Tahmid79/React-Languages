cb695f5e76e7da18dd7bc25713ceb07640dd8629
Tahmid79 <45458895+Tahmid79@users.noreply.github.com>
2020-07-31 23:32:40 +0600

    Language State

---
+++ src/components/App.js
@@ -0,0 +1,26 @@
+import React from 'react' ;
+
+class App extends React.Component{
+
+    state = {language : 'english'} ;
+
+    onLanguageChange = (language) =>{
+        this.setState({language}) ;
+    };
+
+    render() {
+      return (
+         <div className="ui container" >
+           <div>
+               Select a Language
+               <i className="flag us" onClick={()=>this.onLanguageChange('english')} />
+               <i className="flag nl" onClick ={() => this.onLanguageChange('dutch')} />
+           </div>
+             { this.state.language }
+        </div>) ;
+    }
+
+
+}
+
+export default App ;
